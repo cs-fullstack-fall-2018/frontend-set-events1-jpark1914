c692687a5312076ff836405ca1b9e8d6014df594
Jordan Parker <parkerj4321@gmail.com>
2018-08-15 14:49:09 -0500

    Everything but the reset button working

---
+++ js.js
@@ -0,0 +1,31 @@
+//Step 1
+var totalScore = document.getElementById("score");
+var button1 = document.getElementById("theButton");
+var sum = 0;
+button1.addEventListener("click", sumPress);
+
+function sumPress() {
+    sum += 1;
+    totalScore.innerHTML = sum;
+}
+//Step 2
+var totalScore2 = document.getElementById("score2");
+var button2 = document.getElementById("theButton2");
+var sum2 = 0;
+
+button2.addEventListener("click", sumPress2);
+
+function sumPress2() {
+    sum2 += 1;
+    totalScore2.innerHTML = sum2;
+}
+
+//Step 3
+
+var theReset = document.getElementById("reset");
+
+theReset.addEventListener("click",function () {
+    for(var x = totalScore.innerHTML; x >= totalScore; x--){
+        totalScore.innerHTML = totalScore.innerHTML[x];
+    }
+} );
